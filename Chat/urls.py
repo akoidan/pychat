262a19b7304fc61549c0b76bafda4611cacbfc95
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2014-12-21 21:13:19 +0200

    fix home not found, TODO: change /url for {% url %}

--- Chat/urls.py
+++ Chat/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 admin.autodiscover()
 urlpatterns = patterns('',
 		url(r'^admin/', include(admin.site.urls)),
-		url(r'^(home)?$', 'story.views.home', name='home'),
+		url(r'^home$', 'story.views.home'),
+		url(r'^$', 'story.views.home'),
 		# url(r'^login$', 'story.views.login'),
 		url(r'^logout$', 'story.views.logout'),
 		url(r'^auth$', 'story.views.auth'),
