b66f5e2b7f016baf6a4e0335a7d2d90ca9cc2fb4
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2014-12-23 23:58:23 +0200

    Added uncommitted files

---
+++ story/static/js/base.js
@@ -0,0 +1,11 @@
+var sound = true;
+
+function mute() {
+	sound = !sound;
+	var btn = document.getElementById("muteBtn");
+	if (sound) {
+		btn.innerHTML = '<span class="glyphicon glyphicon-volume-up"></span>';
+	} else {
+		btn.innerHTML = '<span class="glyphicon glyphicon-volume-off"></span>';
+	}
+}
