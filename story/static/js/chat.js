137f6ec3ecf2cc2c05fc9203d5cd476f8dda7ff2
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2014-12-23 23:56:19 +0200

    Architecture refactoring

--- story/static/js/chat.js
+++ story/static/js/chat.js
@@ -5,24 +5,11 @@ ishout.on('notifications', appendMessage);
 // WebSocket connection.
 ishout.init();
 
-var sound = true;
-
 function printMessage(data, div) {
 	message = '(' + data.hour + ':' + data.minute + ') <b>' + data.user
 	+ '</b> : ' + data.content;
 	div.append("<p>" + message + "</p>");
 }
-
-function mute() {
-	sound = !sound;
-	var btn = document.getElementById("muteBtn");
-	if (sound) {
-		btn.innerHTML = '<span class="glyphicon glyphicon-volume-up"></span>';
-	} else {
-		btn.innerHTML = '<span class="glyphicon glyphicon-volume-off"></span>';
-	}
-}
-
 
 function appendMessage(data) {
 	loggedUser = $("input#username").val();
