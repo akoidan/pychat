04d3260ca36f32c8ee3d2f4feef6b293e8b3b2a0
deathangel908 <nightmare.quake@mail.com>
2014-12-29 00:28:08 +0200

    Csrf moved to base.js

--- story/static/js/base.js
+++ story/static/js/base.js
@@ -9,3 +9,28 @@ function mute() {
 		btn.innerHTML = '<span class="glyphicon glyphicon-volume-off"></span>';
 	}
 }
+
+// included in all html, because of base.html
+function getCookie(name) {
+	var cookieValue = null;
+	if (document.cookie && document.cookie != '') {
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
+
+$(function () {
+	$.ajaxSetup({
+		beforeSend: function (xhr, settings) {
+			xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+		}
+	});
+});
