06c33c4ec00cc0a90b94e258eb0647224e9c90f3
deathangel908 <nightmare.quake@mail.com>
2015-01-03 19:10:19 +0200

    Added emoticon downloader, user list hover

---
+++ downloadsmiles.py
@@ -0,0 +1,12 @@
+__author__ = 'andrew'
+import re
+from subprocess import call
+call(["wget", "http://emoticons-hangulatjelek.blogspot.com/p/emoticons.html", "-O", "/tmp/smiles.html"])
+
+p = re.compile(r'http:\/\/[^"]*\.gif')
+file = open('/tmp/smiles.html', 'r')
+file_content = file.read()
+result_regex = re.findall(p, file_content)
+call(["mkdir","-p", "/tmp/smiles"])
+for url in result_regex:
+	call(["wget", url, "-P", "/tmp/smiles"])
