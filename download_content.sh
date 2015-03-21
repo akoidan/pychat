b26badee81f6d42491f9ffd674fe4a81a031935d
deathangel908 <nightmare.quake@mail.com>
2015-03-21 13:52:36 +0200

    Improving download content

--- download_content.sh
+++ download_content.sh
@@ -2,79 +2,110 @@
 
 # defining the project structure
 PROJECT_ROOT=`pwd`
+TMP_DIR="/tmp/loaded content/"
 STATIC_DIR="$PROJECT_ROOT/story/static/"
+JS_DIR="$STATIC_DIR/js/"
+CSS_DIR="$STATIC_DIR/css/"
+SOUNDS_DIR="$STATIC_DIR/sounds/"
+FONTS_DIR="$STATIC_DIR/fonts/"
+NODE_DIR="$PROJECT_ROOT/node_modules"
+
+
+# Implementing installed files
+files[1]="$FONTS_DIR/glyphicons-halflings-regular.woff"
+files[2]="$FONTS_DIR/glyphicons-halflings-regular.woff2"
+files[3]="$JS_DIR/jquery.js"
+files[4]="$JS_DIR/jscolor/jscolor.js"
+files[5]="$SOUNDS_DIR/ChatOutgoing.wav"
+files[6]="$SOUNDS_DIR/ChatIncoming.wav"
+files[7]="$CSS_DIR/bootstrap-datepicker.min.css"
+files[8]="$JS_DIR/bootstrap-datepicker.min.js"
+files[9]="$CSS_DIR/bootstrap.min.css"
+files[10]="$JS_DIR/bootstrap.min.js"
+files[11]="$PROJECT_ROOT/node_modules/ishout.js/server.js"
+
+
+# Deleting all content creating empty dirs
+rm -rf $SOUNDS_DIR
+rm -rf $NODE_DIR
+rm -rf $FONTS_DIR
+for path in "${files[@]}" ; do
+  if [ -f $path ]; then
+   rm $path
+  fi
+done
+mkdir $SOUNDS_DIR
+mkdir $FONTS_DIR
+
+# remove tmp directory afterword if It didn't exist
+if [ -d "$TMP_DIR" ]; then
+    clean_tmp_dir=0;
+  else
+    mkdir $TMP_DIR
+    clean_tmp_dir=1;
+fi
+
 cd $PROJECT_ROOT
 
+
 # IShout.js
-echo "Installing ishout.js, this can take a while"
-export PYTHON=python2
-npm install ishout.js
-npm_exit_status=$?
-# The common issue when access denied for ~/.npm directory
-if [[ $npm_exit_status == 3 ]] ; then
-  echo "Seems like you don't have permissions to ~/.npm directory, trying to change it"
-  sudo chown -R $(whoami) ~/.npm
-  npm install ishout.js
-fi;
-unset PYTHON
-rm -rf $PROJECT_ROOT/ishout.js.zip
+command -v npm && {
+  echo "Installing ishout.js, this can take a while" ;
+  export PYTHON=python2 ;
+  npm install ishout.js ;
+  npm_exit_status=$? ;
+  # The common issue when access denied for ~/.npm directory
+  if [[ $npm_exit_status == 3 ]] ; then
+    echo "Seems like you don't have permissions to ~/.npm directory, trying to change it"
+    sudo chown -R $(whoami) ~/.npm
+    npm install ishout.js
+  fi;
+  unset PYTHON
+}
+if [ ! -f "$PROJECT_ROOT/node_modules/ishout.js/server.js" ]; then
+    echo 'Installing ishout.js has failed, trying to install it manualy'
+    rm -rf $NODE_DIR
+    curl -L -o $TMP_DIR/ishout.js.zip https://www.dropbox.com/sh/m0np8p9f7c9cf3k/AACGEDrkpspbSocyQP0SaVk-a?dl=1 && unzip $TMP_DIR/ishout.js.zip -d $NODE_DIR
+fi
 
-curl -L -o ishout.js.zip https://www.dropbox.com/sh/m0np8p9f7c9cf3k/AACGEDrkpspbSocyQP0SaVk-a?dl=1 && unzip ishout.js.zip -d $PROJECT_ROOT/node_modules
 
-#Bootsrap
-mkdir /tmp/bootsrap
-mkdir $STATIC_DIR/fonts/
-wget https://github.com/twbs/bootstrap/releases/download/v3.3.4/bootstrap-3.3.4-dist.zip -O /tmp/bootsrap/bootsrap.zip &&
-unzip /tmp/bootsrap/bootsrap.zip -d /tmp/bootsrap/
-cp /tmp/bootsrap/bootstrap-3.3.4-dist/css/bootstrap.min.css $STATIC_DIR/css/bootstrap.min.css
-cp /tmp/bootsrap/bootstrap-3.3.4-dist/fonts/glyphicons-halflings-regular.woff $STATIC_DIR/fonts/glyphicons-halflings-regular.woff
-cp /tmp/bootsrap/bootstrap-3.3.4-dist/fonts/glyphicons-halflings-regular.woff2 $STATIC_DIR/fonts/glyphicons-halflings-regular.woff2
-cp /tmp/bootsrap/bootstrap-3.3.4-dist/js/bootstrap.min.js $STATIC_DIR/js/bootstrap.min.js
+# Bootsrap
+wget https://github.com/twbs/bootstrap/releases/download/v3.3.4/bootstrap-3.3.4-dist.zip -O $TMP_DIR/bootsrap/bootsrap.zip &&
+unzip $TMP_DIR/bootsrap/bootsrap.zip -d $TMP_DIR/bootsrap/
+cp $TMP_DIR/bootsrap/bootstrap-3.3.4-dist/css/bootstrap.min.css $CSS_DIR/bootstrap.min.css
+cp $TMP_DIR/bootsrap/bootstrap-3.3.4-dist/fonts/glyphicons-halflings-regular.woff $FONTS_DIR/glyphicons-halflings-regular.woff
+cp $TMP_DIR/bootsrap/bootstrap-3.3.4-dist/fonts/glyphicons-halflings-regular.woff2 $FONTS_DIR/glyphicons-halflings-regular.woff2
+cp $TMP_DIR/bootsrap/bootstrap-3.3.4-dist/js/bootstrap.min.js $JS_DIR/bootstrap.min.js
 #datepicker
-wget https://github.com/eternicode/bootstrap-datepicker/blob/master/dist/css/bootstrap-datepicker.min.css#L8 -O $STATIC_DIR/css/bootstrap-datepicker.min.css
-wget https://raw.githubusercontent.com/eternicode/bootstrap-datepicker/master/dist/js/bootstrap-datepicker.min.js -O $STATIC_DIR/js/bootstrap-datepicker.min.js
+wget https://github.com/eternicode/bootstrap-datepicker/blob/master/dist/css/bootstrap-datepicker.min.css#L8 -O $CSS_DIR/bootstrap-datepicker.min.css
+wget https://raw.githubusercontent.com/eternicode/bootstrap-datepicker/master/dist/js/bootstrap-datepicker.min.js -O $JS_DIR/bootstrap-datepicker.min.js
 
 
 # JavaScript
-#wget https://raw.githubusercontent.com/twbs/bootstrap/master/dist/js/bootstrap.js -P $STATIC_DIR/js/
-#wget https://raw.githubusercontent.com/mathiasbynens/he/master/he.js -P $STATIC_DIR/js/
-wget http://code.jquery.com/jquery-2.1.3.min.js -O $STATIC_DIR/js/jquery.js
+#wget https://raw.githubusercontent.com/mathiasbynens/he/master/he.js -P $JS_DIR/
+wget http://code.jquery.com/jquery-2.1.3.min.js -O $JS_DIR/jquery.js
+rm -rf $JS_DIR/jscolor
+wget http://jscolor.com/release/jscolor-1.4.4.zip -P /tmp && unzip $TMP_DIR/jscolor-1.4.4.zip -d $JS_DIR
 
-
-rm -rf $STATIC_DIR/js/jscolor
-wget http://jscolor.com/release/jscolor-1.4.4.zip -P /tmp && unzip /tmp/jscolor-1.4.4.zip -d $STATIC_DIR/js/
-
-# CSS
-#wget https://raw.githubusercontent.com/mzyy94/HONDA-3D-WebGL-demo/master/stylesheets/cssload.css -O $STATIC_DIR/css/confirm-email.css
 
 # Sounds
-rm -rf $STATIC_DIR/sounds
-mkdir $STATIC_DIR/sounds
-curl -L -o /tmp/sounds.zip https://www.dropbox.com/sh/0whi1oo782noit1/AAC-F14YggOFqx3DO3e0AvqGa?dl=1 && unzip /tmp/sounds.zip -d $STATIC_DIR/sounds/
+curl -L -o $TMP_DIR/sounds.zip https://www.dropbox.com/sh/0whi1oo782noit1/AAC-F14YggOFqx3DO3e0AvqGa?dl=1 && unzip $TMP_DIR/sounds.zip -d $SOUNDS_DIR
 
-# Check if all content is loaded
-#folders[1]="$STATIC_DIR/fonts/glyphicons-halflings-regular.svg"
-#folders[2]="$STATIC_DIR/fonts/glyphicons-halflings-regular.eot"
-#folders[3]="$STATIC_DIR/fonts/glyphicons-halflings-regular.ttf"
-folders[4]="$STATIC_DIR/fonts/glyphicons-halflings-regular.woff"
-folders[5]="$STATIC_DIR/js/jquery.js"
-folders[6]="$STATIC_DIR/js/jscolor/jscolor.js"
-folders[8]="$STATIC_DIR/css/confirm-email.css"
-folders[10]="$STATIC_DIR/sounds/ChatOutgoing.wav"
-folders[11]="$STATIC_DIR/sounds/ChatIncoming.wav"
-folders[12]="$STATIC_DIR/css/bootstrap-datepicker.min.css"
-folders[13]="$STATIC_DIR/js/bootstrap-datepicker.min.js"
-folders[14]="$STATIC_DIR/css/bootstrap.min.css"
-folders[15]="$STATIC_DIR/js/bootstrap.min.js"
-folders[16]="$PROJECT_ROOT/node_modules/ishout.js/server.js"
 
+# Checking if all files are loaded
 status="ok"
-for path in "${folders[@]}" ; do
+for path in "${files[@]}" ; do
   if [ ! -f $path ]; then
     status="fail"
     echo "installing $path has failed, download it manually"
   fi
 done
+
+
+if [ $clean_tmp_dir -eq 1 ] ; then
+  rm -rf $TMP_DIR
+fi
+
 
 if [[ $status == "ok" ]] ; then
   echo "Installation succeded"
@@ -82,3 +113,4 @@ else
   echo "Some files haven't been installed, try to install it manually from from https://www.dropbox.com/sh/tr5fr7rl93aghfm/AAAb9obWh6yerSEcmqnGF0NBa?dl=0"
   exit 1
 fi
+
