01391ffe6ce704922c6414a8e2f831f8094f877e
deathangel908 <nightmare.quake@mail.com>
2015-04-10 23:55:03 +0300

    Test for gender

--- story/tests.py
+++ story/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from story.models import UserProfile
+
+
+class ModelTest(TestCase):
+
+	def test_gender(self):
+		user = UserProfile(sex_str='Female')
+		self.assertEqual(user.sex, 2)
+		user.sex_str = 'Male'
+		self.assertEqual(user.sex, 1)
+		user.sex_str = 'WrongString'
+		self.assertEqual(user.sex, 0)
