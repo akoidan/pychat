7c3cc558504d54bb9043441a382c2f78a5c42440
deathangel908 <nightmare.quake@mail.com>
2015-05-16 18:06:07 +0300

    Added managment command

---
+++ story/management/commands/get_model_info.py
@@ -0,0 +1,14 @@
+__author__ = 'andrew'
+from django.core.management import BaseCommand
+from django.db import models
+
+
+class Command(BaseCommand):
+    help = 'Prints all project models and counts objects in the each one'
+
+    can_import_settings = True
+
+    def handle(self, *args, **options):
+        all_models = models.get_models(include_auto_created=True)
+        for member in all_models:
+            self.stdout.write('Table %s has %d rows' % (member.__name__, member.objects.count()))
