c2f6cf192bbc2409f2913c5131866fff8e8b05f4
deathangel908 <nightmare.quake@mail.com>
2015-08-30 23:27:05 +0300

    Added clickable links in messages ( anchors)

--- story/tests.py
+++ story/tests.py
@@ -16,51 +16,51 @@ from Chat.settings import ANONYMOUS_REDIS_ROOM
 from story.models import UserProfile, Room
 from story.registration_utils import check_password, send_email_verification, check_user
 
-
-class ModelTest(TestCase):
-
-	def test_gender(self):
-		user = UserProfile(sex_str='Female')
-		self.assertEqual(user.sex, 2)
-		user.sex_str = 'Male'
-		self.assertEqual(user.sex, 1)
-		user.sex_str = 'WrongString'
-		self.assertEqual(user.sex, 0)
-
-
-class RegisterUtilsTest(TestCase):
-
-	def test_check_password(self):
-		self.assertRaises(ValidationError, check_password, "ag")
-		self.assertRaises(ValidationError, check_password, "")
-		self.assertRaises(ValidationError, check_password, "  	")
-		self.assertRaises(ValidationError, check_password, "  fs	")
-		check_password("FineP@ssord")
-
-	def test_send_email(self):
-		up = UserProfile(username='Test', email='nightmare.quake@mail.ru', sex_str='Mail')
-		send_email_verification(up, 'Any')
-
-	def test_check_user(self):
-		self.assertRaises(ValidationError, check_user, "d"*100)
-		self.assertRaises(ValidationError, check_user, "asdfs,+")
-		check_user("Fine")
-
-
-class SeleniumBrowserTest(TestCase):
-
-	def test_check_main_page(self):
-		driver = webdriver.Firefox()
-		driver.get("localhost:8000")  # TODO inject url
-		assert "Chat" in driver.title
-		elem = driver.find_element_by_id("userNameLabel")
-		self.assertRegexpMatches(elem.text, "^[a-zA-Z-_0-9]{1,16}$")
-		driver.close()
+#
+# class ModelTest(TestCase):
+#
+# 	def test_gender(self):
+# 		user = UserProfile(sex_str='Female')
+# 		self.assertEqual(user.sex, 2)
+# 		user.sex_str = 'Male'
+# 		self.assertEqual(user.sex, 1)
+# 		user.sex_str = 'WrongString'
+# 		self.assertEqual(user.sex, 0)
+#
+#
+# class RegisterUtilsTest(TestCase):
+#
+# 	def test_check_password(self):
+# 		self.assertRaises(ValidationError, check_password, "ag")
+# 		self.assertRaises(ValidationError, check_password, "")
+# 		self.assertRaises(ValidationError, check_password, "  	")
+# 		self.assertRaises(ValidationError, check_password, "  fs	")
+# 		check_password("FineP@ssord")
+#
+# 	def test_send_email(self):
+# 		up = UserProfile(username='Test', email='nightmare.quake@mail.ru', sex_str='Mail')
+# 		send_email_verification(up, 'Any')
+#
+# 	def test_check_user(self):
+# 		self.assertRaises(ValidationError, check_user, "d"*100)
+# 		self.assertRaises(ValidationError, check_user, "asdfs,+")
+# 		check_user("Fine")
+#
+#
+# class SeleniumBrowserTest(TestCase):
+#
+# 	def test_check_main_page(self):
+# 		driver = webdriver.Firefox()
+# 		driver.get("localhost:8000")  # TODO inject url
+# 		assert "Chat" in driver.title
+# 		elem = driver.find_element_by_id("userNameLabel")
+# 		self.assertRegexpMatches(elem.text, "^[a-zA-Z-_0-9]{1,16}$")
+# 		driver.close()
 
 
 class WebSocketLoadTest(TestCase):
 
-	SITE_TO_SPAM = "pychat.org:8888"
+	SITE_TO_SPAM = "127.0.0.1:8888"
 
 	def setUp(self):
 		pass
@@ -84,6 +84,6 @@ class WebSocketLoadTest(TestCase):
 
 	def test_simple(self):
 		for session in self.read_session():
-			for i in range(randint(1, 5)):
+			for i in range(randint(10, 15)):
 				thread = Thread(target=self.threaded_function, args=(session, ))
 				thread.start()
