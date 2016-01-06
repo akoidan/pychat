4ff69a38a1e4ebe3d5a7abc869cf5467c3bd72b7
deathangel908 <deathangel908@gmail.com>
2016-01-07 01:32:41 +0200

    Added log info to md5url

--- chat/templatetags/md5url.py
+++ chat/templatetags/md5url.py
@@ -4,24 +4,26 @@ from os.path import join
 
 from django import template
 
-from chat.settings import STATIC_URL, STATIC_ROOT
+from chat.settings import STATIC_URL, STATIC_ROOT, logging
 
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 
 class UrlCache(object):
 	_md5_sum = {}
-	_counter_lock = threading.Lock()
+	_lock = threading.Lock()
 
 	@classmethod
 	def get_md5(cls, file):
 		try:
 			return cls._md5_sum[file]
 		except KeyError:
-			with cls._counter_lock:
-				md5 = cls.calc_md5(file)
-				value = '%s%s?v=%s' % (STATIC_URL, file, md5[:8])
+			with cls._lock:
+				md5 = cls.calc_md5(file)[:8]
+				value = '%s%s?v=%s' % (STATIC_URL, file, md5)
 				cls._md5_sum[file] = value
+				logger.info("Static file %s calculated md5 %s", file, md5)
 				return value
 
 	@classmethod
