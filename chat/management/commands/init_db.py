e91d478cd74c6ee16cff36900b9a0adf677ea495
deathangel908 <123asd123>
2016-07-02 15:28:32 +0300

    Fixed init_db command

--- chat/management/commands/init_db.py
+++ chat/management/commands/init_db.py
@@ -10,11 +10,10 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		#Django 1.6 South
-		#python manage.py schemamigration story $1 --initial
+		#python manage.py schemamigration chat $1 --initial
 		#python manage.py syncdb --all
 		#python manage.py migrate --fake
-		call_command('migrate')
 		call_command('makemigrations', 'chat')
-		call_command('migrate', 'chat')
+		call_command('migrate')
 		Room(id=ALL_ROOM_ID, name=ALL_REDIS_ROOM).save()
 		# call_command('syncdb')
