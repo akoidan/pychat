9258c30f3b1874db46f6f66d02283ca3f10b2ea8
Deathangel908 <deathangel908@gmail.com>
2016-07-12 02:36:03 +0300

    Removed onaudioprocess after call is finished. Safe processor with  try-catch

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -1716,37 +1716,37 @@ function WebRtcApi() {
 		self.createMicrophoneLevelVoice(stream);
 	};
 	self.createMicrophoneLevelVoice = function(stream) {
-		var AudioContextClass = isFirefox ?  AudioContext : webkitAudioContext;
-		var audioContext = new AudioContextClass();
-		var analyser = audioContext.createAnalyser();
-		var microphone = audioContext.createMediaStreamSource(stream);
-		var javascriptNode = audioContext./*createJavaScriptNode*/createScriptProcessor(2048, 1, 1);
-
-		analyser.smoothingTimeConstant = 0.3;
-		analyser.fftSize = 1024;
-
-		microphone.connect(analyser);
-		analyser.connect(javascriptNode);
-		javascriptNode.connect(audioContext.destination);
-		self.prevVolumeValues = 0;
-		self.volumeValuesCount = 0;
-		javascriptNode.onaudioprocess = function () {
-			var array = new Uint8Array(analyser.frequencyBinCount);
-			analyser.getByteFrequencyData(array);
-			var values = 0;
-			var length = array.length;
-
-			for (var i = 0; i < length; i++) {
-				values += array[i];
+		try {
+			var AudioContextClass = isFirefox ? AudioContext : webkitAudioContext;
+			var audioContext = new AudioContextClass();
+			var analyser = audioContext.createAnalyser();
+			var microphone = audioContext.createMediaStreamSource(stream);
+			self.javascriptNode = audioContext./*createJavaScriptNode*/createScriptProcessor(2048, 1, 1);
+			analyser.smoothingTimeConstant = 0.3;
+			analyser.fftSize = 1024;
+			microphone.connect(analyser);
+			analyser.connect(self.javascriptNode);
+			self.javascriptNode.connect(audioContext.destination);
+			self.prevVolumeValues = 0;
+			self.volumeValuesCount = 0;
+			self.javascriptNode.onaudioprocess = function () {
+				var array = new Uint8Array(analyser.frequencyBinCount);
+				analyser.getByteFrequencyData(array);
+				var values = 0;
+				var length = array.length;
+				for (var i = 0; i < length; i++) {
+					values += array[i];
+				}
+				var value = values / length;
+				self.prevVolumeValues += value;
+				self.volumeValuesCount++;
+				if (self.volumeValuesCount == 300 && self.prevVolumeValues < 10) {
+					console.error(getDebugMessage('Microphone level is too low')); // TODO
+				}
+				self.dom.microphoneLevel.value = value;
 			}
-			var value = values / length;
-			self.prevVolumeValues += value;
-			self.volumeValuesCount++;
-			if (self.volumeValuesCount  == 300 && self.prevVolumeValues < 10) {
-				console.error(getDebugMessage('Microphone level is too low')); // TODO
-			}
-			self.dom.microphoneLevel.value = value;
-
+		} catch (err) {
+			console.error(getDebugMessage("Unable to use microphone level because " + err));
 		}
 	};
 	self.init = function() {
@@ -1801,6 +1801,10 @@ function WebRtcApi() {
 		self.setIconState(false);
 		growlInfo(text);
 		self.hidePhoneIcon();
+		if (self.javascriptNode) {
+			self.javascriptNode.onaudioprocess = null;
+			self.dom.microphoneLevel.value = 0;
+		}
 		self.exitFullScreen(); /*also executes removing event on exiting from fullscreen*/
 	};
 	self.hangUp = function() {
