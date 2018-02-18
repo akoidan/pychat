3ca9330099b302366eb42071206e5fd396e3800d
Andrew Koidan <deathangel908@gmail.com>
2018-02-18 14:28:52 +0200

    Added repr to admin links

--- chat/admin.py
+++ chat/admin.py
@@ -1,23 +1,59 @@
 import inspect
+import json
 import sys
 
+from django.conf import settings
 from django.contrib import admin
-from django.contrib.admin import SimpleListFilter
-from django.core import urlresolvers
-from django.db.models import Count, ForeignKey
+from django.db.models import ForeignKey
 from django.utils.html import format_html
 
 from chat import models
-from chat.models import UserJoinedInfo
-from django.conf import settings
 
 exclude_auto = ()
 model_classes = (class_name[1] for class_name in inspect.getmembers(sys.modules[models.__name__], inspect.isclass)
 					if class_name[1].__module__ == models.__name__ and class_name[0] not in exclude_auto)
+
+def gen_fun(field):
+	def col_name(o):
+		attr = getattr(o, field)
+		v = json.dumps(attr)
+		if len(v) > 50:
+			v = v[:50]
+		return v
+	return col_name
+
+
+main_fields = {
+	'user': gen_fun('username'),
+	'room': gen_fun('name'),
+	'issue': gen_fun('content'),
+	'ip address': gen_fun('ip'),
+	'message': gen_fun('content'),
+	'subscription': gen_fun('id'),
+}
+
+
+def country(instance):
+	iso2 = instance.country_code if instance.country_code else "None"
+	return format_html("<span style='white-space:nowrap'><img src='{}/flags/{}.png' /> {}</span>",
+							 settings.STATIC_URL,
+							 iso2.lower(),
+							 instance.country)
+extra_fields = {
+	'ip address': (country,)
+}
+
+exclude_fields = {
+	'ip address': ('country',)
+}
+
 for model in model_classes:
 	fields = []
+	vname = model._meta.verbose_name
 	class_struct = {'fields': fields, 'list_display': fields}
 	for field in model._meta.fields:
+		if exclude_fields.get(vname) is not None and field.name in extra_fields.get(vname):
+			break
 		if isinstance(field, ForeignKey):
 			def gen_link(field):
 				def link(obj):
@@ -26,8 +62,9 @@ for model in model_classes:
 					if another is None:
 						return "Null"
 					else:
-						link = '/admin/chat/{}/{}/change'.format(another._meta.verbose_name, another.id)
-						return u'<a href="%s">%s</a>' % (link, another.id)
+						another_name = another._meta.verbose_name
+						link = '/admin/chat/{}/{}/change'.format(another_name, another.id)
+						return u'<a href="%s">%s</a>' % (link, main_fields[another_name](another))
 				link.allow_tags = True
 				link.__name__ = str(field.name)
 				return link
@@ -35,6 +72,8 @@ for model in model_classes:
 		else:
 			fields.append(field.name)
 
+	if extra_fields.get(vname) is not None:
+		fields.extend(extra_fields.get(vname))
 	admin.site.register(model, type(
 		'SubClass',
 		(admin.ModelAdmin,),
