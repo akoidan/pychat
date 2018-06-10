52d90fbed1c766e7e571846f93a3e6e7c4d690aa
deathangel908 <deathangel908@gmail.com>
2018-06-10 22:22:52 +0300

    Added html-webpack-plugin

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const chalk = require('chalk');
 
 module.exports = (env, argv) => {
@@ -11,14 +12,7 @@ module.exports = (env, argv) => {
       new VueLoaderPlugin(),
       new MiniCssExtractPlugin(), // extract css to a separate file, instead of having it loaded from js
       // thus we can increase load time, since css is not required for domready state
-      function(){
-        let msg = function(stats) {
-          console.log(chalk.magenta(('\n[' + new Date().toLocaleString() + ']') + ' Begin a new compilation.\n'));
-        };
-        this.plugin('done', msg);
-        this.plugin('failed', msg);
-        this.plugin('invalid', msg);
-      }
+      new HtmlWebpackPlugin({hash: true, favicon: 'src/assets/img/favicon.ico',  template: 'src/index.html'}),
     ],
     resolve: {
       extensions: ['.ts', '.js', '.vue', '.json'],
@@ -64,29 +58,29 @@ module.exports = (env, argv) => {
     },
   };
 
-  if (argv.mode === 'development') {
+  // if (argv.mode === 'development') {
     // create vendor.js file for development so webpack doesn't need to reassemble it every time
     // you can remove `argv.mode === 'development'` if you want it for prod. Or remove this if at all
-    conf.optimization = {
-      splitChunks: {
-        chunks: 'all',
-        minSize: 0,
-        maxAsyncRequests: Infinity,
-        maxInitialRequests: Infinity,
-        name: true,
-        cacheGroups: {
-          vendor: {
-            name: 'vendor',
-            chunks: 'initial',
-            reuseExistingChunk: true,
-            priority: -5,
-            enforce: true,
-            test: /[\\/]node_modules[\\/]/
-          },
-        }
-      }
-    };
-    conf.devtool = '#source-map';
-  }
+    // conf.optimization = {
+    //   splitChunks: {
+    //     chunks: 'all',
+    //     minSize: 0,
+    //     maxAsyncRequests: Infinity,
+    //     maxInitialRequests: Infinity,
+    //     name: true,
+    //     cacheGroups: {
+    //       vendor: {
+    //         name: 'vendor',
+    //         chunks: 'initial',
+    //         reuseExistingChunk: true,
+    //         priority: -5,
+    //         enforce: true,
+    //         test: /[\\/]node_modules[\\/]/ // this also creates single css
+    //       },
+    //     }
+    //   }
+    // };
+    // conf.devtool = '#source-map';
+  // }
   return conf;
 };
