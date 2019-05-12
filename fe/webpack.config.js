eac01196b309f0c9bd84ede2c6ff4d1fa15ce6c5
Andrew Koidan <deathangel908@gmail.com>
2019-05-12 20:48:32 +0300

    Fixed bug

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -44,8 +44,7 @@ module.exports = (env, argv) => {
     let minicssPlugin = {
       loader: MiniCssExtractPlugin.loader,
     };
-g      minicssPlugin.publicPath = options.PUBLIC_PATH;
-    }
+    minicssPlugin.publicPath = options.PUBLIC_PATH;
     sasscPlugins = [
       minicssPlugin,
       'css-loader',
