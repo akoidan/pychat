f2f8fb853af971fe11cff2a1947d6ad3f50173f4
akoidan <deathangel908@gmail.com>
2020-11-25 02:06:58 +0200

    Split ReceivingFileInfo.vue into table separate element

---
+++ frontend/src/vue/chat/ReceivingFileInfo.vue
@@ -0,0 +1,112 @@
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <th>Name:</th>
+        <td>{{ receivingFile.fileName }}</td>
+      </tr>
+      <tr>
+        <th>Size:</th>
+        <td>{{ size }}</td>
+      </tr>
+      <tr>
+        <th>Status:</th>
+        <td>{{ status }}</td>
+      </tr>
+      <tr v-if="receivingFile.error">
+        <th>Error:</th>
+        <td>{{ receivingFile.error }}</td>
+      </tr>
+      <tr v-if="receivingFile.anchor">
+        <th>Download:</th>
+        <td>
+          <a
+            class="green-btn"
+            :href="receivingFile.anchor"
+            :download="receivingFile.fileName"
+          >Save</a>
+        </td>
+      </tr>
+
+      <tr v-if="isError">
+        <th>
+          Retry
+        </th>
+        <td>
+          <i
+            class="icon-repeat"
+            @click="retry"
+          />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue, Watch, Ref} from "vue-property-decorator";
+import { bytesToSize } from '@/ts/utils/pureFunctions';
+import {
+  FileTransferStatus,
+  ReceivingFile
+} from '@/ts/types/model';
+
+@Component
+export default class ReceivingFileInfo extends Vue {
+  @Prop() public receivingFile!: ReceivingFile;
+
+  get size(): string {
+    return bytesToSize(this.receivingFile.upload.total);
+  }
+
+  get isError(): boolean {
+    return this.receivingFile.status === FileTransferStatus.ERROR;
+  }
+
+  public retry() {
+    this.$webrtcApi.retryFile(this.receivingFile.connId, this.receivingFile.opponentWsId);
+  }
+
+  get status(): string {
+    switch (this.receivingFile.status) {
+      case FileTransferStatus.ERROR:
+        return 'Error';
+      case FileTransferStatus.IN_PROGRESS:
+        return 'Downloading...';
+      case FileTransferStatus.DECLINED_BY_OPPONENT:
+        return 'Declined by opponent';
+      case FileTransferStatus.DECLINED_BY_YOU:
+        return 'Declined by you';
+      case FileTransferStatus.FINISHED:
+        return 'Finished';
+      case FileTransferStatus.NOT_DECIDED_YET:
+        return 'Waiting for approval';
+    }
+  }
+}
+</script>
+<!-- eslint-disable -->
+<style lang="sass" scoped>
+
+  table
+    width: 100%
+    text-align: left
+    th
+      color: #79aeb6
+      font-weight: bold
+      padding-left: 5px
+    td
+      text-overflow: ellipsis
+      max-width: 250px
+      overflow: hidden
+      width: 100%
+      padding-left: 10px
+  a
+    width: calc(100% - 50px)
+    display: block
+    text-align: center
+    margin-left: 10px
+    margin-right: 10px
+  .icon-repeat
+    cursor: pointer
+</style>
