a167364682b92bf083076a7a4894f8ee536175a5
akoidan <deathangel908@gmail.com>
2020-11-25 02:06:58 +0200

    Split ReceivingFileInfo.vue into table separate element

--- frontend/src/vue/chat/ChatReceivingFile.vue
+++ frontend/src/vue/chat/ChatReceivingFile.vue
@@ -4,48 +4,9 @@
       :time="receivingFile.time"
       :user-id="receivingFile.userId"
     />
-    <table>
-      <tbody>
-        <tr>
-          <th>Name:</th>
-          <td>{{ receivingFile.fileName }}</td>
-        </tr>
-        <tr>
-          <th>Size:</th>
-          <td>{{ size }}</td>
-        </tr>
-        <tr>
-          <th>Status:</th>
-          <td>{{ status }}</td>
-        </tr>
-        <tr v-if="receivingFile.error">
-          <th>Error:</th>
-          <td>{{ receivingFile.error }}</td>
-        </tr>
-        <tr v-if="receivingFile.anchor">
-          <th>Download:</th>
-          <td>
-            <a
-              class="green-btn"
-              :href="receivingFile.anchor"
-              :download="receivingFile.fileName"
-            >Save</a>
-          </td>
-        </tr>
-
-        <tr v-if="isError">
-          <th>
-            Retry
-          </th>
-          <td>
-            <i
-              class="icon-repeat"
-              @click="retry"
-            />
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <receiving-file-info
+      :receiving-file="receivingFile"
+    />
     <app-progress-bar
       v-if="showProgress"
       class="progress-wrap-file"
@@ -81,50 +42,26 @@ import {
   FileTransferStatus,
   ReceivingFile
 } from '@/ts/types/model';
-import { bytesToSize } from '@/ts/utils/pureFunctions';
 import AppProgressBar from '@/vue/ui/AppProgressBar.vue';
 import ChatMessageHeader from '@/vue/chat/ChatMessageHeader.vue';
+import ReceivingFileInfo from '@/vue/chat/ReceivingFileInfo.vue';
 
 
 @Component({
-  components: {ChatMessageHeader, AppProgressBar}
+  components: {ReceivingFileInfo, ChatMessageHeader, AppProgressBar}
 })
 export default class ChatReceivingFile extends Vue {
   @Prop() public receivingFile!: ReceivingFile;
   @State
   public readonly myId!: number;
-  public FileTransferStatus = FileTransferStatus;
 
   get showYesNo(): boolean {
     return this.receivingFile.status === FileTransferStatus.NOT_DECIDED_YET;
   }
-  get size(): string {
-    return bytesToSize(this.receivingFile.upload.total);
-  }
+
 
   get showProgress(): boolean {
     return FileTransferStatus.IN_PROGRESS === this.receivingFile.status;
-  }
-
-  get isError(): boolean {
-    return this.receivingFile.status === FileTransferStatus.ERROR;
-  }
-
-  get status(): string {
-    switch (this.receivingFile.status) {
-      case FileTransferStatus.ERROR:
-        return 'Error';
-      case FileTransferStatus.IN_PROGRESS:
-        return 'Downloading...';
-      case FileTransferStatus.DECLINED_BY_OPPONENT:
-        return 'Declined by opponent';
-      case FileTransferStatus.DECLINED_BY_YOU:
-        return 'Declined by you';
-      case FileTransferStatus.FINISHED:
-        return 'Finished';
-      case FileTransferStatus.NOT_DECIDED_YET:
-        return 'Waiting for approval';
-    }
   }
 
   get mainClass(): string {
@@ -133,10 +70,6 @@ export default class ChatReceivingFile extends Vue {
     } else {
       return 'message-others';
     }
-  }
-
-  public retry() {
-    this.$webrtcApi.retryFile(this.receivingFile.connId, this.receivingFile.opponentWsId);
   }
 
   public accept() {
@@ -151,29 +84,6 @@ export default class ChatReceivingFile extends Vue {
 </script>
 
 <style lang="sass" scoped>
-
-  .icon-repeat
-    cursor: pointer
-
-  a
-    width: calc(100% - 50px)
-    display: block
-    text-align: center
-    margin-left: 10px
-    margin-right: 10px
-  table
-    width: 100%
-    text-align: left
-    th
-      color: #79aeb6
-      font-weight: bold
-      padding-left: 5px
-    td
-      text-overflow: ellipsis
-      max-width: 250px
-      overflow: hidden
-      width: 100%
-      padding-left: 10px
 
   .progress-wrap-file /deep/ .progress-wrap
     width: calc(100% - 40px)
