706d3170edb70bf8c855d91b448883fdc1409b50
Andrew Koidan <deathangel908@gmail.com>
2021-02-22 16:19:20 +0200

    Fix manifest

--- frontend/src/ts/main.ts
+++ frontend/src/ts/main.ts
@@ -256,6 +256,10 @@ async function init() {
   // }
 
 }
+window.addEventListener('beforeinstallprompt', function(e) {
+  e.preventDefault();
+  window.deferredPrompt = (e as any);
+});
 
 if (document.readyState !== 'loading') {
   init();
