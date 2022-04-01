798571424a9671c3b25726856c23cba130aba201
akoidan <deathangel908@gmail.com>
2022-04-01 18:00:56 +0300

    Fix

--- frontend/src/ts/instances/storeInstance.ts
+++ frontend/src/ts/instances/storeInstance.ts
@@ -1,10 +1,25 @@
 import type { VueBase } from 'vue-class-component';
-import { getModule } from 'vuex-module-decorators';
-import { stateDecoratorFactory } from 'vuex-module-decorators-state';
+import { getModule,VuexModule } from 'vuex-module-decorators';
 import { DefaultStore } from '@/ts/classes/DefaultStore';
-import { IS_DEBUG } from '@/ts/utils/consts';
 import { encodeHTML } from '@/ts/utils/htmlApi';
 import { GrowlType } from '@/ts/types/model';
+
+
+function stateDecoratorFactory<TPT extends VuexModule>(vuexModule: TPT):
+    <TCT extends (TCT[TPN] extends TPT[TPN] ? unknown : never), TPN extends (keyof TCT & keyof TPT)>
+    (vueComponent: TCT, fileName: TPN) => void {
+  return <TCT extends (TCT[TPN] extends TPT[TPN] ? unknown : never), TPN extends (keyof TCT & keyof TPT)>
+  (vueComponent: TCT, fileName: TPN): void => {
+    Object.defineProperty(
+        vueComponent,
+        fileName,
+        Object.getOwnPropertyDescriptor(
+            vuexModule,
+            fileName,
+        ) as PropertyDescriptor,
+    );
+  };
+}
 
 
 export const store: DefaultStore = getModule(DefaultStore);
