a6bab3efffd836a2f39c94261acb3e2fcea54b1b
akoidan <deathangel908@gmail.com>
2023-05-26 21:27:19 +0300

    Fix terraform chart

--- kubernetes/terraform/helm/main.tf
+++ kubernetes/terraform/helm/main.tf
@@ -15,7 +15,47 @@ provider "helm" {
   }
 }
 
+resource "helm_release" "global" {
+  name = "backend"
+  chart = "./charts/global"
+}
+
 resource "helm_release" "backend" {
   name = "backend"
   chart = "./charts/backend"
+}
+
+resource "helm_release" "certmanager" {
+  name = "certmanager"
+  chart = "./charts/certmanager"
+}
+
+resource "helm_release" "coturn" {
+  name = "coturn"
+  chart = "./charts/coturn"
+}
+
+resource "helm_release" "frontend" {
+  name = "frontend"
+  chart = "./charts/frontend"
+}
+
+resource "helm_release" "ingress" {
+  name = "ingress"
+  chart = "./charts/ingress"
+}
+
+resource "helm_release" "mariadb" {
+  name = "mariadb"
+  chart = "./charts/mariadb"
+}
+
+resource "helm_release" "postfix" {
+  name = "postfix"
+  chart = "./charts/postfix"
+}
+
+resource "helm_release" "redis" {
+  name = "redis"
+  chart = "./charts/redis"
 }
