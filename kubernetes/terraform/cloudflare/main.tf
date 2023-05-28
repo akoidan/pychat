19fb5a44c2b7efb0ec7daddabdc58ade372a2172
akoidan <deathangel908@gmail.com>
2023-05-29 01:32:25 +0300

    Fix domain 3rd level

--- kubernetes/terraform/cloudflare/main.tf
+++ kubernetes/terraform/cloudflare/main.tf
@@ -8,7 +8,7 @@ resource "cloudflare_record" "master_pychat" {
 }
 
 resource "cloudflare_record" "static_pychat" {
-  name    = "static"
+  name    = "static.${var.domain_name}"
   proxied = true
   ttl     = 1
   type    = "A"
@@ -18,7 +18,7 @@ resource "cloudflare_record" "static_pychat" {
 
 resource "cloudflare_record" "registry_pychat" {
   count = var.htpasswd == null ? 0 : 1
-  name    = "registry"
+  name    = "registry.${var.domain_name}"
   proxied = false
   ttl     = 1
   type    = "A"
